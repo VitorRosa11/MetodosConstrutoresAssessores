bfd9fdd7c8d586b520786d8b6d9d881fcb143985
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-04 10:58:54 -0300

    Add files via upload

---
+++ Atividade04.java
@@ -0,0 +1,18 @@
+
+
+package metodosconstrutoresassessores.atividade04;
+
+/**
+ *
+ * @author vitorb
+ */
+public class Atividade04 {
+
+    public static void main(String[] args) {
+          Retangulo retangulo1 = new Retangulo(5.0, 3.0);
+          
+        retangulo1.exibirInformacoes();
+    }
+}
+
+
---
+++ Retangulo04.java
@@ -0,0 +1,37 @@
+
+package metodosconstrutoresassessores.atividade04;
+
+/**
+ *
+ * @author vitorb
+ */
+public class Retangulo04 {
+    
+ private double largura;
+    private double altura;
+
+    public Retangulo(double largura, double altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double calcularArea() {
+        return largura * altura;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Largura: " + largura);
+        System.out.println("Altura: " + altura);
+        System.out.println("Área: " + calcularArea());
+    }
+}
+
+
